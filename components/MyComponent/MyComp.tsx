4b4b37bbca3b39c733bdd7ec165ced5d752369a8
Phaxe <Yahia.eid@outlook.com>
2025-02-17 19:44:31 +0200

    s

--- components/MyComponent/MyComp.tsx
+++ components/MyComponent/MyComp.tsx
@@ -6,8 +6,8 @@ const MyComp: React.FC = () => {
   return (
     <div className="flex flex-col gap-2 bg-gray-200 p-10 rounded-lg shadow-md">
       <h1 className="text-2xl font-bold">Welcome</h1>
-      <p>
-        This is a simple demo demonstrating CRUD operations like in a real-life project.
+      <p className="text-center text-gray-700 md:w-[50%] mx-auto">
+        This side project is a full-stack web application for managing order issues, built with Next.js 15, Tailwind CSS, TypeScript, Redux, Redux Toolkit, Shadcn, Formik, Yup, Axios, and Lucide Icons. It provides a dynamic, real-time interface for tracking and updating orders efficiently.
       </p>
       <div className="flex itemc justify-center gap-5 mt-2">
         <Button asChild className='bg-[#151b23]'>
